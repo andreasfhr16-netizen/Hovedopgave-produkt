9c04ce7a7cd11b42cf0057c6dc89d43d720c381f
Andreas <andreasfhr16@gmail.com>
2026-08-11 23:03:28 +0200

    Another commit

--- my-app/next.config.ts
+++ my-app/next.config.ts
@@ -2,6 +2,20 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+
+
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/frontpage',
+        permanent: true,
+      },
+    ]
+  },
+
+
 };
 
 export default nextConfig;
+
